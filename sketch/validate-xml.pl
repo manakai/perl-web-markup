715d7c53edc996dba765a27d693f0de20f5f4f56
Wakaba <wakaba@suikawiki.org>
2015-01-05 17:05:42 +0900

    Updated validator tests

---
+++ sketch/validate-xml.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+use Path::Class;
+use lib glob file (__FILE__)->dir->parent->subdir ('t_deps', 'modules', '*', 'lib');
+use Web::DOM::Document;
+use Web::XML::Parser;
+use Web::HTML::Validator;
+
+my $doc = new Web::DOM::Document;
+my $parser = Web::XML::Parser->new;
+$parser->scripting (1);
+$parser->locale_tag (lc $ENV{LANG}) if $ENV{LANG};
+
+local $/ = undef;
+$parser->parse_byte_string (undef, scalar <> => $doc);
+
+my $checker = new Web::HTML::Validator;
+$checker->scripting ($parser->scripting);
+$checker->check_node ($doc);
